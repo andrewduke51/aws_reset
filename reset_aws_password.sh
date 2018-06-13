92c2611048af54bc69860a3afb8395a8b1ae3436
andrewduke51 <andrew.figaroa@snagajob.com>
2018-06-13 15:40:58 -0400

    add decsicion

--- reset_aws_password.sh
+++ reset_aws_password.sh
@@ -2,9 +2,9 @@
 
 echo "Hello, set your new password for the aws console :-)?"
 read PASSWORD
-#echo "what is your username?"
-# read USER
-echo "Choose your aws profile!"
+echo "what is your username?"
+read USER
+echo "Choose your aws profile? (or just press enter to accept the default)"
 read PROFILE
 aws iam update-login-profile --user-name $USER --password $PASSWORD --profile $PROFILE
 echo "You are all set and you can login to the aws console using your new password now!"
